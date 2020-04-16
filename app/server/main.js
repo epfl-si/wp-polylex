839beb5e699a78ef07ab9fc6d3826008ec4ecb98
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-16 14:01:53 +0200

    EvalError: Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive: script-src 'self' 'unsafe-inline'

--- app/server/main.js
+++ app/server/main.js
@@ -1,73 +1,79 @@
 import helmet from "helmet";
 import Tequila from "meteor/epfl:accounts-tequila";
-import { Meteor } from 'meteor/meteor';
-import { WebApp } from 'meteor/webapp';
-import { importData } from './import-data';
-import { AppLogger } from '../imports/api/logger';
-import '../imports/api/methods/publications';
-import './rest-api';
-import '../imports/api/methods/responsibles';
-import '../imports/api/methods/categories';
-import '../imports/api/methods/subcategories';
-import '../imports/api/methods/lexes';
+import { Meteor } from "meteor/meteor";
+import { WebApp } from "meteor/webapp";
+import { importData } from "./import-data";
+import { AppLogger } from "../imports/api/logger";
+import "../imports/api/methods/publications";
+import "./rest-api";
+import "../imports/api/methods/responsibles";
+import "../imports/api/methods/categories";
+import "../imports/api/methods/subcategories";
+import "../imports/api/methods/lexes";
 
 Meteor.startup(() => {
-  
-    let needImportData = true;
-    let activeTequila = true;
+  let needImportData = true;
+  let activeTequila = true;
 
-    // Define lang <html lang="fr" />
-    WebApp.addHtmlAttributeHook(() => ({ lang: 'fr' }));
+  // Define lang <html lang="fr" />
+  WebApp.addHtmlAttributeHook(() => ({ lang: "fr" }));
 
-    
-    // https://guide.meteor.com/security.html#httpheaders
-    WebApp.connectHandlers.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          defaultSrc: ["'self'"],
-          scriptSrc: ["'self'", "'unsafe-inline'"],
-          connectSrc: ["*"],
-          imgSrc: ["'self'"],
-          styleSrc: ["'self'", "'unsafe-inline'"],
-        },
-        browserSniff: false
-      })
-    );
-    
-    // https://guide.meteor.com/security.html#xframeoptions
-    WebApp.connectHandlers.use(helmet.frameguard());  // defaults to sameorigin
+  // https://guide.meteor.com/security.html#httpheaders
+  WebApp.connectHandlers.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        // TODO: How to remove "'unsafe-eval'" and make form validation work ?
+        scriptSrc: ["'self'", "'unsafe-inline'", 
+          "'unsafe-eval'"
+        ],
+        connectSrc: ["*"],
+        imgSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+      },
+      browserSniff: false,
+    })
+  );
 
-    // Setting up logs
-    new AppLogger();
+  // https://guide.meteor.com/security.html#xframeoptions
+  WebApp.connectHandlers.use(helmet.frameguard()); // defaults to sameorigin
 
-    if (needImportData) {
-      importData();
-    }
+  // Setting up logs
+  new AppLogger();
 
-    if (activeTequila) {
+  if (needImportData) {
+    importData();
+  }
 
-      Tequila.start({
-        service: 'Polylex',
-        request: ['uniqueid', 'email', 'group'],
-        bypass: ['/api'],
-        getUserId(tequila) {
-          let groups = tequila.group.split(",");
-          if (groups.includes('wp-polylex-admins')) {
-            Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP);
-          } else if (groups.includes('wp-polylex-editors')) {
-            Roles.setUserRoles(tequila.uniqueid, ['editor'], Roles.GLOBAL_GROUP);
-          } else {
-            Roles.setUserRoles(tequila.uniqueid, ['epfl-member'], Roles.GLOBAL_GROUP);
-          }
-          if (tequila.uniqueid == "188475") {
-            Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP); 
-          }
-          return tequila.uniqueid;
-        },
-        upsert: (tequila) => ({ $set: {
+  if (activeTequila) {
+    Tequila.start({
+      service: "Polylex",
+      request: ["uniqueid", "email", "group"],
+      bypass: ["/api"],
+      getUserId(tequila) {
+        let groups = tequila.group.split(",");
+        if (groups.includes("wp-polylex-admins")) {
+          Roles.setUserRoles(tequila.uniqueid, ["admin"], Roles.GLOBAL_GROUP);
+        } else if (groups.includes("wp-polylex-editors")) {
+          Roles.setUserRoles(tequila.uniqueid, ["editor"], Roles.GLOBAL_GROUP);
+        } else {
+          Roles.setUserRoles(
+            tequila.uniqueid,
+            ["epfl-member"],
+            Roles.GLOBAL_GROUP
+          );
+        }
+        if (tequila.uniqueid == "188475") {
+          Roles.setUserRoles(tequila.uniqueid, ["admin"], Roles.GLOBAL_GROUP);
+        }
+        return tequila.uniqueid;
+      },
+      upsert: (tequila) => ({
+        $set: {
           username: tequila.user,
-          emails: [ tequila.email ],
-        }}),
-      });
-    }
+          emails: [tequila.email],
+        },
+      }),
+    });
+  }
 });
