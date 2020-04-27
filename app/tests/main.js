b219ed51bc3b285b252474ae0934e6a3ed86b31d
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-27 15:30:12 +0200

    [test] unit tests

--- app/tests/main.js
+++ app/tests/main.js
@@ -2,6 +2,7 @@ import assert from "assert";
 import "../imports/api/methods/tests/categories.test";
 import "../imports/api/methods/tests/subcategories.test";
 import "../imports/api/methods/tests/responsibles.test";
+import "../imports/api/methods/tests/lexes.test";
 
 describe("wp-polylex", function () {
   it("package.json has correct name", async function () {
