b618ef80e833dfc318c6f5ca1efa86588b8aced8
Grégory Charmier <gregory.charmier@gmail.com>
2020-05-04 14:32:15 +0200

    delete publish roles

--- app/imports/api/publications.js
+++ app/imports/api/publications.js
@@ -1,31 +1,33 @@
-import { Lexes, Categories, Subcategories, Responsibles, AppLogs } from "./collections";
+import {
+  Lexes,
+  Categories,
+  Subcategories,
+  Responsibles,
+  AppLogs,
+} from "./collections";
 
 if (Meteor.isServer) {
-  Meteor.publish('lexes', function() {  
+  Meteor.publish("lexes", function () {
     return Lexes.find();
   });
 
-  Meteor.publish('categories', function() {
+  Meteor.publish("categories", function () {
     return Categories.find();
   });
 
-  Meteor.publish('subcategories', function() {
+  Meteor.publish("subcategories", function () {
     return Subcategories.find();
   });
 
-  Meteor.publish('responsibles', function () {
-    return Responsibles.find();    
-  })
+  Meteor.publish("responsibles", function () {
+    return Responsibles.find();
+  });
 
-  Meteor.publish('users', function () { 
-      return Meteor.users.find({});
+  Meteor.publish("users", function () {
+    return Meteor.users.find({});
   });
-/*
-  Meteor.publish('user.roles', function () {
-      return Meteor.roles.find({});
-  });
-*/
-  Meteor.publish('log.list', function() {    
+
+  Meteor.publish("log.list", function () {
     return AppLogs.find({});
   });
 
