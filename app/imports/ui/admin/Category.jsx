ef5f27de5b5cfc356bbbd4821ba8a566701e7d95
Julien Delasoie <julien.delasoie@epfl.ch>
2022-09-28 16:40:43 +0200

    Rework category form

--- app/imports/ui/admin/Category.jsx
+++ app/imports/ui/admin/Category.jsx
@@ -1,10 +1,9 @@
-import React, { Component, Fragment } from "react";
-import { Meteor } from "meteor/meteor";
-import { withTracker } from "meteor/react-meteor-data";
+import React, { useState, useEffect } from "react";
+import { useFind, useSubscribe } from 'meteor/react-meteor-data'
 import { Formik, Field, ErrorMessage } from "formik";
-import { Categories } from "../../api/collections";
+import { Categories } from '../../api/collections'
 import { CustomError, CustomInput } from "../CustomFields";
-import { Link } from "react-router-dom";
+import { Link, useParams } from 'react-router-dom'
 import { AlertSuccess, Loading } from "../Messages";
 import {
   insertCategory,
@@ -12,251 +11,225 @@ import {
   removeCategory,
 } from "../../api/methods/categories";
 
-class CategoriesList extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h5 className="card-header">Liste des rubriques</h5>
-        <ul className="list-group">
-          {this.props.categories.map((category, index) => (
-            <li
-              key={category._id}
-              value={category.nameFr}
-              className="list-group-item"
-            >
-              {category.nameFr} / {category.nameEn}
-              <button type="button" className="close" aria-label="Close">
-                <span
-                  onClick={() => {
-                    if (
-                      window.confirm(
-                        "Are you sure you wish to delete this item?"
-                      )
-                    )
-                      this.props.callBackDeleteCategory(category._id);
-                  }}
-                  aria-hidden="true"
-                >
-                  &times;
-                </span>
-              </button>
-              <Link
-                className="edit"
-                to={`/admin/category/${category._id}/edit`}
-              >
-                <button type="button" className="btn btn-outline-primary">
-                  Éditer
-                </button>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
-  }
-}
 
-class Category extends Component {
-  constructor(props) {
-    super(props);
+export const Category = () => {
+  const isLoading = useSubscribe('responsibles');
+  const { _id } = useParams();
 
-    let action;
-    if (this.props.match.path == "/admin/category/:_id/edit") {
-      action = "edit";
-    } else {
-      action = "add";
+  window.history.replaceState({}, document.title)
+
+  const [addSuccess, setAddSuccess] = useState(false);
+  const [editSuccess, setEditSuccess] = useState(false);
+  const [deleteSuccess, setDeleteSuccess] = useState(false);
+
+  const clearUserMsg = () => {
+    setAddSuccess(false);
+    setEditSuccess(false);
+    setDeleteSuccess(false);
+  };
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      // After 3 seconds, clear notifications
+      clearUserMsg();
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeId);
     }
+  }, []);
 
-    this.state = {
-      action: action,
-      addSuccess: false,
-      editSuccess: false,
-      deleteSuccess: false,
-    };
+  const addCategory = (values, actions) => {
+    insertCategory.call(values, (errors) => {
+      if (errors) {
+        console.log(errors);
+        let formErrors = {};
+        errors.details.forEach(function(error) {
+          formErrors[error.name] = error.message;
+        });
+        actions.setErrors(formErrors);
+        actions.setSubmitting(false);
+      } else {
+        actions.setSubmitting(false);
+        actions.resetForm();
+        setAddSuccess(true);
+      }
+    });
   }
 
-  deleteCategory = (categoryId) => {
-    removeCategory.call({ categoryId }, (error, categoryId) => {
-      if (error) {
-        console.log(`ERROR Category removeCategory ${error}`);
-        alert(error);
+  const editCategory = (values, actions) => {
+    updateCategory.call(values, (errors) => {
+      if (errors) {
+        console.log(errors);
+        let formErrors = {};
+        errors.details.forEach(function (error) {
+          formErrors[error.name] = error.message;
+        });
+        actions.setErrors(formErrors);
+        actions.setSubmitting(false);
       } else {
-        this.setState({ deleteSuccess: true });
+        actions.setSubmitting(false);
+        setEditSuccess(true);
       }
     });
   };
 
-  updateUserMsg = () => {
-    this.setState({
-      addSuccess: false,
-      editSuccess: false,
-      deleteSuccess: false,
+  const deleteCategory = (categoryId) => {
+    removeCategory.call({ categoryId }, (error) => {
+      if (error) {
+        console.log(`ERROR Category removeCategory ${error}`);
+        alert(error);
+      } else {
+        setDeleteSuccess(true);
+      }
     });
   };
 
-  submitCategory = (values, actions) => {
-    if (this.state.action === "add") {
-      insertCategory.call(values, (errors, CategoryId) => {
-        if (errors) {
-          console.log(errors);
-          let formErrors = {};
-          errors.details.forEach(function (error) {
-            formErrors[error.name] = error.message;
-          });
-          actions.setErrors(formErrors);
-          actions.setSubmitting(false);
-        } else {
-          actions.setSubmitting(false);
-          actions.resetForm();
-          this.setState({ addSuccess: true });
+  if (isLoading()) {
+    return <Loading/>
+  } else {
+    return (
+      <>
+        { _id ?
+          <h5 className="card-header">
+            Édition de la rubrique
+          </h5> :
+          <CategoriesList
+              callBackDeleteCategory={deleteCategory}
+          />
         }
-      });
-    } else if (this.state.action === "edit") {
-      updateCategory.call(values, (errors, CategoryId) => {
-        if (errors) {
-          console.log(errors);
-          let formErrors = {};
-          errors.details.forEach(function (error) {
-            formErrors[error.name] = error.message;
-          });
-          actions.setErrors(formErrors);
-          actions.setSubmitting(false);
-        } else {
-          actions.setSubmitting(false);
-          this.setState({ editSuccess: true });
-        }
-      });
-    }
-  };
-
-  getCategory = () => {
-    // Get the URL parameter
-    let categoryId = this.props.match.params._id;
-    let category = Categories.findOne({ _id: categoryId });
-    return category;
-  };
-
-  getInitialValues = () => {
-    let initialValues;
-    if (this.state.action == "add") {
-      initialValues = { nameFr: "", nameEn: "" };
-    } else {
-      initialValues = this.getCategory();
-    }
-    return initialValues;
-  };
-
-  render() {
-    let content;
-    let initialValues = this.getInitialValues();
-    let isLoading =
-      this.props.categories == undefined || initialValues == undefined;
-
-    if (isLoading) {
-      content = <Loading />;
-    } else {
-      const isDisplayCategoriesList = this.state.action == "add";
-
-      content = (
-        <Fragment>
-          {this.state.deleteSuccess ? (
+        <div className="card-body">
+          { addSuccess &&
+            <AlertSuccess
+              message={"La nouvelle rubrique a été ajoutée avec succès !"}
+            />
+          }
+          { editSuccess &&
+            <AlertSuccess
+              message={"La rubrique a été modifiée avec succès !"}
+            />
+          }
+          { deleteSuccess &&
             <AlertSuccess
               message={"La rubrique a été supprimée avec succès !"}
             />
-          ) : null}
-
-          {isDisplayCategoriesList ? (
-            <CategoriesList
-              categories={this.props.categories}
-              callBackDeleteCategory={this.deleteCategory}
+          }
+          { <CategoryForm
+              category={ _id ? Categories.findOne({ _id: _id }) : null }
+              onSubmit={ _id ? editCategory : addCategory }
+              clearUserMsg={ clearUserMsg }
             />
-          ) : (
-            <h5 className="card-header">
-              Édition de la rubrique des Lexes suivante:{" "}
-            </h5>
-          )}
-          <div className="card-body">
-            {this.state.addSuccess ? (
-              <AlertSuccess
-                message={"La nouvelle rubrique a été ajoutée avec succès !"}
-              />
-            ) : null}
-
-            {this.state.editSuccess ? (
-              <AlertSuccess
-                message={"La rubrique a été modifiée avec succès !"}
-              />
-            ) : null}
-
-            {this.state.deleteSuccess ? (
-              <AlertSuccess
-                message={"La rubrique a été supprimée avec succès !"}
-              />
-            ) : null}
-
-            <Formik
-              onSubmit={this.submitCategory}
-              initialValues={initialValues}
-              validateOnBlur={false}
-              validateOnChange={false}
-            >
-              {({ handleSubmit, isSubmitting, handleChange, handleBlur }) => (
-                <form onSubmit={handleSubmit}>
-                  <Field
-                    onChange={(e) => {
-                      handleChange(e);
-                      this.updateUserMsg();
-                    }}
-                    onBlur={(e) => {
-                      handleBlur(e);
-                      this.updateUserMsg();
-                    }}
-                    placeholder="Nom de la rubrique en français à ajouter"
-                    name="nameFr"
-                    type="text"
-                    component={CustomInput}
-                  />
-                  <ErrorMessage name="nameFr" component={CustomError} />
-
-                  <Field
-                    onChange={(e) => {
-                      handleChange(e);
-                      this.updateUserMsg();
-                    }}
-                    onBlur={(e) => {
-                      handleBlur(e);
-                      this.updateUserMsg();
-                    }}
-                    placeholder="Nom de la rubrique en anglais à ajouter"
-                    name="nameEn"
-                    type="text"
-                    component={CustomInput}
-                  />
-                  <ErrorMessage name="nameEn" component={CustomError} />
-
-                  <div className="my-1 text-right">
-                    <button
-                      type="submit"
-                      disabled={isSubmitting}
-                      className="btn btn-primary"
-                    >
-                      Enregistrer
-                    </button>
-                  </div>
-                </form>
-              )}
-            </Formik>
-          </div>
-        </Fragment>
-      );
-    }
-    return content;
+          }
+        </div>
+      </>
+    );
   }
 }
 
-export default withTracker(() => {
-  Meteor.subscribe("categories");
-  return {
-    categories: Categories.find({}, { sort: { nameFr: 1 } }).fetch(),
+export const CategoriesList = (props) => {
+  const isLoading = useSubscribe('categories');
+  const categories = useFind(() => Categories.find({}, { sort: { nameFr: 1 } }), []);
+
+  if (isLoading()) {
+    return <Loading />;
+  } else {
+    return (
+        <>
+          <h5 className="card-header">Liste des rubriques</h5>
+          <ul className="list-group">
+            { categories.map((category) => (
+                <li
+                    key={ category._id }
+                    value={ category.nameFr }
+                    className="list-group-item"
+                >
+                  { category.nameFr } / { category.nameEn }
+                  <button type="button" className="close" aria-label="Close">
+              <span
+                  onClick={ () => { window.confirm("Are you sure you wish to delete this item?") &&
+                  props.callBackDeleteCategory(category._id);
+                  } }
+                  aria-hidden="true"
+              >
+                &times;
+              </span>
+                  </button>
+                  <Link
+                      className="edit"
+                      to={ `/admin/category/${ category._id }/edit` }
+                  >
+                    <button type="button" className="btn btn-outline-primary">
+                      Éditer
+                    </button>
+                  </Link>
+                </li>
+            )) }
+          </ul>
+        </>
+    );
+  }
+}
+
+const CategoryForm = ({ category, onSubmit, clearUserMsg }) => {
+  const getInitialValues = (category) => {
+    return category ? category : { nameFr: "", nameEn: "" };
   };
-})(Category);
+
+  return (
+      <Formik
+          enableReinitialize={true}
+          onSubmit={onSubmit}
+          initialValues={getInitialValues(category)}
+          validateOnBlur={false}
+          validateOnChange={false}
+      >
+        {({ handleSubmit, isSubmitting, handleChange, handleBlur }) => (
+            <form onSubmit={handleSubmit}>
+              <Field
+                  onChange={(e) => {
+                    handleChange(e);
+                    clearUserMsg();
+                  }}
+                  onBlur={(e) => {
+                    handleBlur(e);
+                    clearUserMsg();
+                  }}
+                  placeholder="Nom de la rubrique en français à ajouter"
+                  name="nameFr"
+                  type="text"
+                  component={CustomInput}
+              />
+              <ErrorMessage name="nameFr" component={CustomError} />
+
+              <Field
+                  onChange={(e) => {
+                    handleChange(e);
+                    clearUserMsg();
+                  }}
+                  onBlur={(e) => {
+                    handleBlur(e);
+                    clearUserMsg();
+                  }}
+                  placeholder="Nom de la rubrique en anglais à ajouter"
+                  name="nameEn"
+                  type="text"
+                  component={CustomInput}
+              />
+              <ErrorMessage name="nameEn" component={CustomError} />
+
+              <div className="my-1 text-right">
+                <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="btn btn-primary"
+                >
+                  Enregistrer
+                </button>
+              </div>
+            </form>
+        )}
+      </Formik>
+  );
+}
+
+export default Category;
