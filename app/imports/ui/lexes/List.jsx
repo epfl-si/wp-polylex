2a9f4105c3a2012e1ac321624643dcd066d2e3b8
Julien Delasoie <julien.delasoie@epfl.ch>
2022-09-29 15:12:34 +0200

    Redo lexes list and form

--- app/imports/ui/lexes/List.jsx
+++ app/imports/ui/lexes/List.jsx
@@ -1,78 +1,73 @@
-import React, { Component } from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data'
 import { Link } from 'react-router-dom';
-import { Lexes, Categories, Subcategories, Responsibles } from '../../api/collections';
+import { Lexes, Categories, Subcategories, Responsibles } from '../../api/collections'
 import { Loading } from '../Messages';
 import moment from 'moment';
 import { removeLex } from '../../api/methods/lexes';
 
-class Cells extends Component {
-
-  getCategoryNameFr = (categoryId) => {
+const Cells = (props) => {
+  const getCategoryNameFr = (categoryId) => {
     let categoryNameFr;
-    for(const category of this.props.categories) {
-      if (category._id == categoryId) {
+    for(const category of props.categories) {
+      if (category._id === categoryId) {
         categoryNameFr = category.nameFr;
-        break;   
+        break;
       }
     }
     return categoryNameFr;
   }
 
-  getSubcategoryNameFr = (subcategoryId) => {
+  const getSubcategoryNameFr = (subcategoryId) => {
     let subcategoryNameFr;
-    for(const subcategory of this.props.subcategories) {
-      if (subcategory._id == subcategoryId) {
+    for(const subcategory of props.subcategories) {
+      if (subcategory._id === subcategoryId) {
         subcategoryNameFr = subcategory.nameFr;
-        break;   
+        break;
       }
     }
     return subcategoryNameFr;
   }
 
-  render() {
-    return (
+  return (
       <tbody>
-        {this.props.lexes.map( (lex, index) => (
+      {props.lexes.map( (lex) => (
           <tr key={lex._id}>
             <td><a href={lex.urlFr} target="_blank">{lex.lex}</a></td>
             <td>{lex.titleFr}</td>
             <td>{moment(lex.effectiveDate).format('DD-MM-YYYY')}</td>
             <td>{moment(lex.revisionDate).format('DD-MM-YYYY')}</td>
-            <td>{this.getCategoryNameFr(lex.categoryId)}</td>
-            <td>{this.getSubcategoryNameFr(lex.subcategoryId)}</td>
+            <td>{getCategoryNameFr(lex.categoryId)}</td>
+            <td>{getSubcategoryNameFr(lex.subcategoryId)}</td>
             <td>
               <Link className="mr-2" to={`/edit/${lex._id}`}>
                 <button type="button" className="btn btn-outline-primary">Éditer</button>
               </Link>
-              <button 
-                type="button" 
-                className="btn btn-outline-primary" 
-                onClick={ () => { if (window.confirm('Are you sure you wish to delete this item?')) this.props.deleteLex(lex._id) }}
-                >Supprimer</button>
+              <button
+                  type="button"
+                  className="btn btn-outline-primary"
+                  onClick={ () => { if (window.confirm('Are you sure you wish to delete this item?')) this.props.deleteLex(lex._id) }}
+              >Supprimer</button>
             </td>
           </tr>
-        ))}
+      ))}
       </tbody>
-    )
-  }
+  )
 }
 
-class List extends Component {
-  
-  deleteLex = (lexId) => {
+export const List = ({isLoading, lexes, categories, subcategories}) => {
+  const deleteLex = (lexId) => {
     removeLex.call(
-      {lexId}, 
-      function(error, lexId) {
-        if (error) {
-          console.log(`ERROR removeLex ${error}`);
+        {lexId},
+        function(error) {
+          if (error) {
+            console.log(`ERROR removeLex ${error}`);
+          }
         }
-      }
     );
   }
 
-  export = () => {
-    let lexes = this.props.lexes;
+  const exporter = () => {
     lexes.forEach(function (lex) {
       // Responsible info
       let responsible = Responsibles.findOne({ _id: lex.responsibleId });
@@ -111,56 +106,58 @@ class List extends Component {
     saveAs(blob, "polylex.csv");
   }
 
-  render() {
-    let content;
-    let isLoading = (this.props.categories == undefined || this.props.lexes == undefined);
-    if (isLoading) {
-      content = <Loading />;
-    } else {
-      content = (
-        <div className="">
-          <h4 className="py-3 float-left">Polylex</h4>
-          <div className="mt-1 text-right">
-            <button onClick={(e) => this.export(e)} className="btn btn-primary">
-              Exporter CSV
-            </button>
-          </div>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">LEX</th>
-                <th scope="col">Title</th>
-                <th scope="col" className="w-25">Date d'entrée en vigueur</th>
-                <th scope="col" className="w-25">Date de révision</th>
-                <th scope="col">Rubrique</th>
-                <th scope="col">Sous-rubrique</th>
-                <th className="w-50">Actions</th>
-              </tr>
-            </thead>
-            <Cells 
-              lexes={this.props.lexes} 
-              categories={this.props.categories} 
-              subcategories={this.props.subcategories} 
-              deleteLex={ this.deleteLex }/>
-          </table>
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="">
+        <h4 className="py-3 float-left">Polylex</h4>
+        <div className="mt-1 text-right">
+          <button onClick={(e) => exporter(e)} className="btn btn-primary">
+            Exporter CSV
+          </button>
         </div>
-      )
-    }
-    return content;
+        <table className="table table-striped">
+          <thead>
+          <tr>
+            <th scope="col">LEX</th>
+            <th scope="col">Title</th>
+            <th scope="col" className="w-25">Date d'entrée en vigueur</th>
+            <th scope="col" className="w-25">Date de révision</th>
+            <th scope="col">Rubrique</th>
+            <th scope="col">Sous-rubrique</th>
+            <th className="w-50">Actions</th>
+          </tr>
+          </thead>
+          <Cells
+              lexes={lexes}
+              categories={categories}
+              subcategories={subcategories}
+              deleteLex={ deleteLex }/>
+        </table>
+      </div>
+    )
   }
-}
-export default withTracker(() => {
-  Meteor.subscribe('lexes');
-  Meteor.subscribe('categories');
-  Meteor.subscribe('subcategories');
-  Meteor.subscribe('responsibles');
 
-  let lexes = Lexes.find({}).fetch();
-  
+
+}
+
+export default withTracker(() => {
+  const handles = [
+    Meteor.subscribe('lexes'),
+    Meteor.subscribe('categories'),
+    Meteor.subscribe('subcategories'),
+    Meteor.subscribe('responsibles'),
+  ];
+
+  const areHandlesReady = () => {
+    return handles.every((handle) => handle.ready());
+  }
+
   return {
-    lexes: lexes,
+    isLoading: !areHandlesReady(),
+    lexes: Lexes.find({}).fetch(),
     categories: Categories.find({}).fetch(),
     subcategories: Subcategories.find({}).fetch(),
-    responsibles: Responsibles.find({}).fetch(),
   };
 })(List);
