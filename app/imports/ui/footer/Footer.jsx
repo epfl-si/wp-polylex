0f96b54b2bfa4efb8704e827087d4483778dccc1
Julien Delasoie <julien.delasoie@epfl.ch>
2023-01-31 10:51:34 +0100

    Bump copyright year

--- app/imports/ui/footer/Footer.jsx
+++ app/imports/ui/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () =>
   <footer className="small border-top">
-    <div className="footer-copyright text-center py-3">© 2022 Copyright
+    <div className="footer-copyright text-center py-3">© 2023 Copyright
       <a href="https://www.epfl.ch"> EPFL</a>
     </div>
   </footer>
