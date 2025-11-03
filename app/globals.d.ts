3f3a148ca98d65be6efda2aff52a846e789dbb61
Julien Delasoie <julien.delasoie@epfl.ch>
2025-11-03 14:59:23 +0100

    feat: Add entra auth

--- app/globals.d.ts
+++ app/globals.d.ts
@@ -6,3 +6,17 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+
+/**
+ * Declare the account package connecting through Entra
+ *
+ * @see {@link https://github.com/epfl-si/meteor-account-entra}
+ */
+declare module Meteor {
+  function entraSignIn(
+    options?: {
+      requestPermissions: string[];
+    },
+    callBack?: (error: any) => void
+  ): void
+}
